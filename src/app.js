1d7e7f406fed2330d2a6e18494d24e75dc05ceb2
portiap1983 <portia@infofit.ca>
2023-03-14 19:01:06 -0700

    updated icons

--- src/app.js
+++ src/app.js
@@ -33,16 +33,19 @@ function displayTemperature (response){
     let humidityElement = document.querySelector ("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector ("#icon")
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 cityElement.innerHTML = response.data.city;
 description.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = response.data.temperature.humidity;
 windElement.innerHTML =Math.round (response.data.wind.speed);
-dateElement.innerHTML = formatDate(response.data.time+1000);
+dateElement.innerHTML = formatDate(response.data.time * 1000);
+iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}png`);
+iconElement.setAttribute("alt", response.data.condition.description)
 }
 
 let apiKey = "5a032ao3cfdb5cb2a245077a27fe06ft";
-let city = "Las Vegas";
+let city = "Vancouver";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
 
 axios.get(apiUrl).then(displayTemperature);
